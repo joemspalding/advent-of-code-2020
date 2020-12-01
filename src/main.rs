387d3c0830497b04320eef960e13937c8d6f7342
joemspalding <jmspal11@louisville.edu>
2020-12-01 17:06:55 -0500

    day one-expense report

---
+++ src/main.rs
@@ -0,0 +1,8 @@
+mod lib;
+
+fn main() {
+    println!("{:?}", lib::day_one::find_2020_product_two_num().unwrap());
+    println!("{:?}", lib::day_one::find_2020_product_three_num().unwrap());
+
+
+}
