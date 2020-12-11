e7f0d89fa2dc15a52a6f046b89b0da60899433fa
joemspalding <jmspal11@louisville.edu>
2020-12-11 13:30:41 -0500

    day 6 complete!

--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,10 @@ fn main() {
     // println!("trees in all patterns: {}", day_three::find_trees_all_patterns());'
     // println!("valid passports: {}", day_four::count_valid_passports());
     // println!("valid passports with valid data: {}", day_four::count_valid_passports_with_valid_data());
-    println!("{:?}", day_five::get_max_seat_score());
-    println!("{:?}", day_five::get_my_seat_score());
+    // println!("{:?}", day_five::get_max_seat_score());
+    // println!("{:?}", day_five::get_my_seat_score());
+    println!("{:?}", day_six::get_answers_count_sum());
+    println!("{:?}", day_six::get_all_answers());
 
 
 }
